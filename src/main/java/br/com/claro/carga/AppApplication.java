e17b6f0830bc952c833063afcb737c7ef57378b6
Vinicius Martins Passos <vinicius.passos@sysmap.com.br>
2023-07-14 14:15:15 -0300

    Ajuste pom.xml

--- src/main/java/br/com/claro/carga/AppApplication.java
+++ src/main/java/br/com/claro/carga/AppApplication.java
@@ -2,10 +2,8 @@ package br.com.claro.carga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"br.com.claro.persistence","br.com.claro.carga"})
 public class AppApplication {
 
 	public static void main(String[] args) {
